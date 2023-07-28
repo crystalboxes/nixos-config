991a9f7d429554227d27d0ba7ff47f529b5ae47d
crystalboxes <crystalboxesgfx@gmail.com>
2023-07-28 11:07:31 +0300

    update

--- users/mitchellh/home-manager.nix
+++ users/mitchellh/home-manager.nix
@@ -168,25 +168,25 @@ in {
 
   programs.git = {
     enable = true;
-    userName = "Mitchell Hashimoto";
-    userEmail = "mitchell.hashimoto@gmail.com";
-    signing = {
-      key = "523D5DC389D273BC";
-      signByDefault = true;
-    };
-    aliases = {
-      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-      root = "rev-parse --show-toplevel";
-    };
-    extraConfig = {
-      branch.autosetuprebase = "always";
-      color.ui = true;
-      core.askPass = ""; # needs to be empty to use terminal for ask pass
-      credential.helper = "store"; # want to make this more secure
-      github.user = "mitchellh";
-      push.default = "tracking";
-      init.defaultBranch = "main";
-    };
+    userName = "crystalboxes";
+    userEmail = "crystalboxesgfx@gmail.com";
+    # signing = {
+    #   key = "523D5DC389D273BC";
+    #   signByDefault = true;
+    # };
+    # aliases = {
+    #   prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+    #   root = "rev-parse --show-toplevel";
+    # };
+    # extraConfig = {
+    #   branch.autosetuprebase = "always";
+    #   color.ui = true;
+    #   core.askPass = ""; # needs to be empty to use terminal for ask pass
+    #   credential.helper = "store"; # want to make this more secure
+    #   github.user = "mitchellh";
+    #   push.default = "tracking";
+    #   init.defaultBranch = "main";
+    # };
   };
 
   programs.go = {
