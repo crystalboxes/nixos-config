7b58b7919c249304fd2624444af531614493136e
crystalboxes <crystalboxesgfx@gmail.com>
2023-07-28 11:08:05 +0300

    update password

--- users/mitchellh/nixos.nix
+++ users/mitchellh/nixos.nix
@@ -12,7 +12,7 @@
     home = "/home/snowbear";
     extraGroups = [ "docker" "wheel" ];
     shell = pkgs.fish;
-    hashedPassword = "$6$p5nPhz3G6k$6yCK0m3Oglcj4ZkUXwbjrG403LBZkfNwlhgrQAqOospGJXJZ27dI84CbIYBNsTgsoH650C1EBsbCKesSVPSpB1";
+    hashedPassword = "$6$mWY0NrRF8dI4mpE2$UfIr7b1PwMHq40fpgoEAXMz/ZyCJCGBFaQsNp5XF6vphPWxmwAz.Ema1yhgnULmIHqLJYtEdlAIRJDEQKExwa1";
     # openssh.authorizedKeys.keys = [
     #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbTIKIPtrymhvtTvqbU07/e7gyFJqNS4S0xlfrZLOaY mitchellh"
     # ];
